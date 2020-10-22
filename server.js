f5578bd06796ce4bb305f6f995371a88901e7d4e
Jane Ermolaev <jane.ermolaev@appsflyer.com>
2020-10-22 20:26:22 +0300

    add GET to find contact and GET to return all contacts

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+const { constants } = require('buffer')
 const express = require('express')
 const app = express()
 const port = 3000
 
+let contacts = [{'Jane':'1234'}, {'Dave':'1878788'}, {'Oren':'45435'}, {'Ben':'4308345098345'}]
+
 app.get('/whos-there', (req, res) => {
   res.send('Hi Trax! This is Jane')
 })
+
+app.get('/get-all-contacts', (req, res) => {
+    res.send(contacts)
+  })
+
+app.get('/find-contact/:contactName', (req, res) => {
+    let name = req.params.contactName
+    contacts.forEach(element => {
+        if (Object.keys(element)[0] === name){
+            res.send(element)
+        }
+    })
+    res.send()
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
